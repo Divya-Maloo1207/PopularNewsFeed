00d075f6790beec771efa55005dd63435fe4a035
Divya Maloo <maloodivya25@gmail.com>
2022-07-02 19:08:09 +0530

    first commit

---
+++ PopularNewsFeed/PopularNewsFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PopularNewsFeed
+//
+//  Created by Divya Maloo on 02/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PopularNewsFeed/PopularNewsFeedUITests/PopularNewsFeedUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PopularNewsFeedUITestsLaunchTests.swift
+//  PopularNewsFeedUITests
+//
+//  Created by Divya Maloo on 02/07/22.
+//
+
+import XCTest
+
+class PopularNewsFeedUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
